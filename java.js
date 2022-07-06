06faac91137aede5260d1b18b9b0a4586a3d7562
Krish6633 <krishkevadiya6633@gmail.com>
2022-07-06 14:12:35 +0530

    Initial Commit

---
+++ java.js
@@ -0,0 +1,26 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("navheight");
+
+window.onscroll = function(){
+    if(window.pageYOffset >= menu.offsetTop){
+        navbar.classList.add("sticky");
+
+    }
+    else{
+        navbar.classList.remove('sticky');
+    }
+}
+
+var item = document.getElementById("MAIN");
+
+  window.addEventListener("wheel", function (e) {
+    if (e.deltaY > 0) item.scrollLeft += 100;
+    else item.scrollLeft -= 100;
+  });
